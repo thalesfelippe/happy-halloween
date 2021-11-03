08cb29ea7a73241d60be8fa89e5d6d02ed009aad
thales felippe <thales_so@hotmail.com>
2021-11-03 00:37:15 -0300

    category and fixed header

--- js/main.js
+++ js/main.js
@@ -37,7 +37,11 @@ let homeSwiper = new Swiper(".home-swiper", {
 });
 
 /* CHANGE BACKGROUND HEADER */
-
+function scrollHeader(){
+    const header = document.getElementById('header')
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /* NEW SWIPER */
 
